8bb863a613271a922dbe1980ab3334434d4a3617
PaulaEscobar <aanitaescobar@gmail.com>
2023-11-10 18:50:57 -0300

    formulario mercado liebre css

--- app.js
+++ app.js
@@ -5,11 +5,7 @@ const path = require("path");
 app.listen(3030, (req, res) => 
     console.log("puerto corriendo exitoso"))
 
-// const publicPath= path.resolve(__dirname, "./public");
-// app.use(express.static(publicPath));
 
-
-// app.use(express.static("public"));
 
 
 app.use (express.static("public"));
@@ -21,5 +17,11 @@ app.use (express.static("public"));
 
 
     app.get("/registro",(req, res) => {
-        res.sendFile(path.join(__dirname,"/views/regitro.html"))
+        res.sendFile(path.join(__dirname,"/views/registro.html"))
     });
+
+    
+    app.get('/login',(req,res)=> {
+        res.sendFile(path.join(__dirname,'/views/login.html'));
+        console.log(__dirname)
+    })
